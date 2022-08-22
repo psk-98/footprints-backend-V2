a551f29e6442e6a3043b90f3c9948e22df948dcd
psk-98 <paulk198@hotmail.com>
2022-08-22 16:32:17 +0200

    fix staicfiles

--- store/settings.py
+++ store/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
