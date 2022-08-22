aaa21871c1a994a192fde70bed58ebf27fac0ea1
psk-98 <paulk198@hotmail.com>
2022-08-22 19:42:15 +0200

    fix whitenoise

--- store/settings.py
+++ store/settings.py
@@ -53,6 +53,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
